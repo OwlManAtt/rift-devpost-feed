608d3afa7160f065426e76ab8f848c322ffc6852
Nicholas 'OwlManAtt' Evans <owlmanatt@gmail.com>
2011-02-24 04:52:15 +0000

    Produces a feed.

--- test.rb
+++ test.rb
@@ -1,37 +1,31 @@
+#!/usr/bin/ruby
 require 'rubygems'
 require 'classifier'
-require 'sanitize'
+require 'nokogiri'
 require 'rss'
-require 'builder'
 
 # Sample data
-rss = RSS::Parser.parse(open('sample.rss'), false)
-training_data = {
-  0 => :moderation,
-  1 => :moderation,
-  2 => :moderation,
-  3 => :moderation,
-  4 => :moderation,
-  5 => :moderation,
-  6 => :noteworthy,
-  7 => :noteworthy,
-}
-
 b = Classifier::Bayes.new 'moderation', 'noteworthy' 
-#training_data.each {|i,category| b.train category.to_s, rss.items[i].description}
+xml = Nokogiri::XML(open('training.xml'))
+xml.search('/root/data').each do |data|
+  b.train data.search('category').first.content, data.search('text').first.content
+end
 
-xml = ''
-builder = Builder::XmlMarkup.new(:target => xml, :indent => 2)
-builder.instruct!(:xml, :encoding => "UTF-8")
-builder.root do |r| 
-  train = training_data.map do |i,category| 
-    r.data do |d|
-      d.category category.to_s
-      d.text Sanitize.clean(rss.items[i].description)
+rj_feed = RSS::Parser.parse(open('http://feeds.feedburner.com/RiftJunkies-RiftDeveloperTrackerFeed'), false)
+clean_feed = RSS::Maker.make('2.0') do |m|
+  m.channel.title = 'Rift Dev Tracker (via RiftJunkies)'
+  m.channel.link = 'http://www.riftjunkies.com/dev-tracker/'
+  m.channel.description = 'A cleaned-up RiftJunkies devtracker feed. Moderation-related posts (thread cleanup/closure notices) should be gone.'
+
+  rj_feed.items.each do |item|
+    unless b.classify(item.description) == 'moderation'
+      i = m.items.new_item
+      i.title = item.title
+      i.link = item.link
+      i.description = item.description
+      i.date = item.date    
     end
   end
 end
 
-File.open('training.xml', 'w') {|f| f.write(xml) }
-
-#puts b.classify rss.items[14].description
+File.open('current.rss',"w") {|f| f.write(clean_feed) }
