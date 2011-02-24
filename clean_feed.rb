2de04e1625a9ea353a5d3c59f7f17f0d48edb518
Nicholas 'OwlManAtt' Evans <owlmanatt@gmail.com>
2011-02-24 05:21:11 +0000

    Fixed some stuff, improved HTML cleanup

--- clean_feed.rb
+++ clean_feed.rb
@@ -2,15 +2,31 @@
 require 'rubygems'
 require 'classifier'
 require 'nokogiri'
+require 'sanitize'
 require 'rss'
 
-# Sample data
+# Set up a decent HTML sanitizer (to purge the feedburner ad bullshit)
+SANITIZE_CONF = {
+  :elements => %w[
+    b blockquote br cite code em i li mark ol p pre
+    s small strike strong sub sup u ul var
+  ],
+  :attributes => {
+    'blockquote' => ['cite'],
+  },
+  :protocols => {
+    'blockquote' => {'cite' => ['http', 'https', :relative]},
+  }
+}
+
+# Train the filter. 
 b = Classifier::Bayes.new 'moderation', 'noteworthy' 
 xml = Nokogiri::XML(open('training.xml'))
 xml.search('/root/data').each do |data|
-  b.train data.search('category').first.content, data.search('text').first.content
+  b.train data.search('category').first.content.to_s, data.search('text').first.content.to_s
 end
 
+# Parse the feed and copy it to a new feed.
 rj_feed = RSS::Parser.parse(open('http://feeds.feedburner.com/RiftJunkies-RiftDeveloperTrackerFeed'), false)
 clean_feed = RSS::Maker.make('2.0') do |m|
   m.channel.title = 'Rift Dev Tracker (via RiftJunkies)'
@@ -18,11 +34,11 @@ clean_feed = RSS::Maker.make('2.0') do |m|
   m.channel.description = 'A cleaned-up RiftJunkies devtracker feed. Moderation-related posts (thread cleanup/closure notices) should be gone.'
 
   rj_feed.items.each do |item|
-    unless b.classify(item.description) == 'moderation'
+    unless b.classify(item.description).downcase == 'moderation'
       i = m.items.new_item
       i.title = item.title
       i.link = item.link
-      i.description = item.description
+      i.description = Sanitize.clean(item.description, SANITIZE_CONF)
       i.date = item.date    
     end
   end
